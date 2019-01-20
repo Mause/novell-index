b5865cf68d86c98c32a98340ce180b11d0504bf9
Dominic May <me@mause.me>
2019-01-20 16:34:02 +0800

    Add test

---
+++ test.py
@@ -0,0 +1,24 @@
+import os
+import tempfile
+
+import pytest
+from main import app
+
+
+@pytest.fixture
+def client():
+    db_fd, app.app.config["DATABASE"] = tempfile.mkstemp()
+    app.app.config["TESTING"] = True
+    client = app.app.test_client()
+
+    with app.app.app_context():
+        app.init_db()
+
+    yield client
+
+    os.close(db_fd)
+    os.unlink(app.app.config["DATABASE"])
+
+
+def test_simple(client):
+    client.get("/")
